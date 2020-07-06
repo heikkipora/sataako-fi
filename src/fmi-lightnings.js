9cf507f51c09263cc0181702088b4ff9266e52d3
Heikki Pora <heikki.pora@gmail.com>
2020-07-06 21:55:13 +0300

    Speed up lightning parsing about 1000x by ditching lodash and especially the mostly useless uniqWith() call

--- src/fmi-lightnings.js
+++ src/fmi-lightnings.js
@@ -53,27 +53,26 @@ function xmlToObject(xml) {
   return parseStringPromise(xml, {tagNameProcessors: [stripPrefix, firstCharLowerCase]})
 }
 
-function extractLocationsAndTimes(queryResult) {
-  // No lightnings atm
-  if (!_.has(queryResult.featureCollection, 'member')) {
+function extractLocationsAndTimes({featureCollection}) {
+  if (!featureCollection.member) {
+    // No lightnings observed in this collection period
     return []
   }
-  return _(queryResult.featureCollection.member[0].gridSeriesObservation[0]
-    .result[0].multiPointCoverage[0].domainSet[0].simpleMultiPoint[0].positions[0]
+
+  const positions = featureCollection.member[0].gridSeriesObservation[0].result[0].multiPointCoverage[0].domainSet[0].simpleMultiPoint[0].positions[0]
+  return positions
     .split('\n')
-    .map(_.trim))
-    .map(pos =>
-    ({
-      // Pos is (lat, lon) - GeoJSON in client is (lon, lat)
-      location: pos.split(' ').slice(0, 2).map(parseFloat).reverse(),
-      time: new Date(parseInt(pos.split(' ')[2], 10) * 1000)
-    }))
-    .uniqWith(_.isEqual)
-    .value()
+    .map(pos => {
+      const [lat, lon, seconds] = pos.trim().split(' ')
+      return {
+        location: [parseFloat(lon), parseFloat(lat)],
+        time: new Date(parseInt(seconds, 10) * 1000)
+      }
+    })
 }
 
 function snapLightningsToFrames(lightnings, frameDates) {
-  return frameDates.map((frame) => ({
+  return frameDates.map(frame => ({
       timestamp: frame.toISOString(),
       locations: _.remove(lightnings, ({time}) => time < frame).map(({location}) => location)
     })
