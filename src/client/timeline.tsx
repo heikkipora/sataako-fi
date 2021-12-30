e98b2b49d7db0091624f1c4d36fe8aabb46c1275
Heikki Pora <heikki.pora@gmail.com>
2021-12-30 13:32:54 +0200

    Refactor Timeline component to modern React

--- src/client/timeline.tsx
+++ src/client/timeline.tsx
@@ -1,88 +1,69 @@
 import classNames from 'classnames'
 import {format, parseISO} from 'date-fns'
-import React from 'react'
 import {Frame} from './types'
+import React, {useCallback} from 'react'
 
-interface TimelineProps {
-  currentTimestamp: string
-  running: boolean
-  timestamps: Frame[]
-  onResume: () => void
-  onSelect: (timestamp: string) => void
+export function Timeline({currentTimestamp, running, timestamps, onResume, onSelect}: {currentTimestamp: string, running: boolean, timestamps: Frame[], onResume: () => void, onSelect: (timestamp: string) => void}) {
+  const touchHandler = useCallback(event => onTouch(onSelect, event), [onSelect])
+  const touchEndHandler = useCallback(event => onTouchEnd(onResume, event), [onResume])
+
+  return <div className="timeline" onTouchStart={touchHandler} onTouchMove={touchHandler} onTouchEnd={touchEndHandler}>
+    {timestamps.map(({timestamp, isForecast}) => <Tick key={timestamp} timestamp={timestamp} isCurrent={timestamp === currentTimestamp} isForecast={isForecast} running={running} onResume={onResume} onSelect={onSelect}/>)}
+  </div>
 }
 
-export class Timeline extends React.PureComponent<TimelineProps> {
-  render() {
-    const {timestamps, currentTimestamp} = this.props
-    const touchHandler = this.onTouch.bind(this)
-    const touchEndHandler = this.onTouchEnd.bind(this)
-    return <div className="timeline" onTouchStart={touchHandler} onTouchMove={touchHandler} onTouchEnd={touchEndHandler}>
-      {this.renderTicks(timestamps, currentTimestamp)}
-    </div>
+function onTouch(onSelect: (timestamp: string) => void, event: React.TouchEvent) {
+  event.preventDefault()
+  if (event.touches.length === 0) {
+    return
   }
 
-  renderTicks(timestamps: Frame[], currentTimestamp: string) {
-    return timestamps.map(t =>
-      this.renderTick(t, t.timestamp === currentTimestamp)
-    )
+  const {clientX, clientY} = event.touches.item(0)
+  const element = document.elementFromPoint(clientX, clientY)
+  if (element instanceof HTMLElement) {
+    const {timestamp} = element.dataset
+    if (timestamp) {
+      onSelect(timestamp)
+    }
   }
+}
 
-  renderTick({timestamp, isForecast}: Frame, isCurrent: boolean) {
-    const formattedTimestamp = this.formatTimestamp(timestamp)
-    const quarter = this.isQuarter(formattedTimestamp)
-    const className = classNames(
-      'timeline__tick',
-      {'timeline__tick--large': quarter},
-      {'timeline__tick--small': !quarter},
-      {'timeline__tick--selected': !this.props.running && isCurrent}
-    )
-    const onSelectHandler = this.props.onSelect.bind(null, timestamp)
-    const onEnterHandler = this.onMouseEnter.bind(this, timestamp)
-    return <div className={className} onMouseDown={onSelectHandler} onMouseUp={this.props.onResume} onMouseEnter={onEnterHandler} key={timestamp} data-timestamp={timestamp}>
-      {isCurrent && this.renderTooltip(formattedTimestamp, isForecast)}
-    </div>
-  }
+function onTouchEnd(onResume: () => void, event: React.TouchEvent) {
+  event.preventDefault()
+  onResume()
+}
 
-  onMouseEnter(timestamp: string, event: React.MouseEvent) {
+function Tick({isCurrent, isForecast, running, timestamp, onResume, onSelect}: {isCurrent: boolean, isForecast?: boolean, running: boolean, timestamp: string, onResume: () => void, onSelect: (timestamp: string) => void}) {
+  const formattedTimestamp = format(parseISO(timestamp), 'HH:mm')
+  const quarter = isQuarter(formattedTimestamp)
+  const className = classNames(
+    'timeline__tick',
+    {'timeline__tick--large': quarter},
+    {'timeline__tick--small': !quarter},
+    {'timeline__tick--selected': !running && isCurrent}
+  )
+  const onSelectHandler = useCallback(() => onSelect(timestamp), [timestamp, onSelect])
+  const onEnterHandler = useCallback(event => {
     const leftPressed = event.buttons === 1
     if (leftPressed) {
-      this.props.onSelect(timestamp)
+      onSelect(timestamp)
     }
-  }
+  }, [timestamp, onSelect])
 
-  onTouch(event: React.TouchEvent) {
-    event.preventDefault()
-    if (event.touches.length === 0) {
-      return
-    }
+  return <div className={className} onMouseDown={onSelectHandler} onMouseUp={onResume} onMouseEnter={onEnterHandler} key={timestamp} data-timestamp={timestamp}>
+    {isCurrent && renderTooltip(formattedTimestamp, isForecast)}
+  </div>
+}
 
-    const {clientX, clientY} = event.touches.item(0)
-    const element = document.elementFromPoint(clientX, clientY)
-    if (element instanceof HTMLElement) {
-      const {timestamp} = element.dataset
-      if (timestamp) {
-        this.props.onSelect(timestamp)
-      }
-    }
-  }
+function isQuarter(formattedTimestamp: string) {
+  const [, minutes] = formattedTimestamp.split(':')
+  return ['00', '15', '30', '45'].includes(minutes)
+}
 
-  onTouchEnd(event: React.TouchEvent) {
-    event.preventDefault()
-    this.props.onResume()
-  }
-
-  renderTooltip = (formattedTimestamp: string, isForecast?: boolean) => {
-    const className = classNames(
-      'timeline__tooltip',
-      {'timeline__tooltip--forecast': isForecast}
-    )
-    return <div className={className}>{formattedTimestamp}</div>
-  }
-
-  formatTimestamp = (timestamp: string) => format(parseISO(timestamp), 'HH:mm')
-
-  isQuarter = (formattedTimestamp: string) => {
-    const [, minutes] = formattedTimestamp.split(':')
-    return ['00', '15', '30', '45'].includes(minutes)
-  }
+function renderTooltip(formattedTimestamp: string, isForecast?: boolean) {
+  const className = classNames(
+    'timeline__tooltip',
+    {'timeline__tooltip--forecast': isForecast}
+  )
+  return <div className={className}>{formattedTimestamp}</div>
 }
